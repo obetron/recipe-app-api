911a7e575bca13019aa1af87fb1d96aba27a1e02
obetron <erenbasaran@gmail.com>
2019-06-07 23:57:25 +0300

    PEP 8 standards applyed.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -8,8 +9,8 @@ class ModelTests(TestCase):
         email = 'test@gelecex.com'
         password = 'Test123'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+            email=email,
+            password=password
         )
 
         self.assertEqual(user.email, email)
