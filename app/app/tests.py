c4088214e8feb2cb3a0a79350dc5ea15ee1a04af
obetron <erenbasaran@gmail.com>
2019-06-07 22:01:52 +0300

    unit test added for substraction, and writing substraction method.

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,7 +1,12 @@
 from django.test import TestCase
-from app.calc import add
+from app.calc import add, substract
+
 
 class MyTestCase(TestCase):
     def test_add(self):
         """Test that two numbers are added together"""
         self.assertEqual(add(11, 3), 14)
+
+    def test_substract(self):
+        """Test that values are substracted and returned"""
+        self.assertEqual(substract(5, 11), 6)
